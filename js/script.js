0aa29ec2fb492ec4651a7208c387cda37e515b84
Otomi19 <oo6661@thewcs.org>
2019-11-14 22:57:05 +0000

    day 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 // *************************** YOUR CODE BELOW *******************************
+$("#shoot").click(function(){
+ var result=$("#input").val();
+ $("#userChoice").text(result);
+ //the computer's choice
+ var fortune= Math.random();
+ if(fortune>.65&&fortune<.7){
+     $("#computerChoice").text("paper");
+ }
+ else if (fortune>.39 && fortune<.65){
+    $("#computerChoice").text("rock");
+ }
+ else if (fortune>.01 && fortune<.39){
+    $("#computerChoice").text("scissors");
+ }
+ else{
+      $("#computerChoice").text("redoo..you cheated");
+ };
+
+});
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
